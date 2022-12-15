308748ebc3e416e621f55916a9b03b513202bd25
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2022-12-15 14:20:28 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module api-crud
+
+go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	envTest := os.Getenv("HELLO")
+
+	fmt.Println(envTest)
+}
