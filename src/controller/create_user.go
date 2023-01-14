329554a24c219effb5688fa6715940904fad6df8
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2023-01-14 14:45:19 -0300

    validation errors

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,9 +1,8 @@
 package controller
 
 import (
-	"api-crud/src/configuration/rest_err"
+	"api-crud/src/configuration/validation"
 	"api-crud/src/controller/model/request"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,8 +11,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error))
-
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
